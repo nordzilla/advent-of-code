4bfb800a0689e83d41edd880fc8b364213471e3d
Erik Nordin <nordzilla@mozilla.com>
2021-12-01 12:50:10 -0800

    Add clever solution to day 01

--- 2020/aoc-rust/src/main.rs
+++
@@ -1,3 +0,0 @@
-use aoc_runner_derive::aoc_main;
-
-aoc_main! { lib = aoc_rust_2020 }
--- 2021/aoc-rust/src/day01.rs
+++ 2021/aoc-rust/src/day01.rs
@@ -1,28 +1,30 @@
 use aoc_runner_derive::*;
 
-type InputPart1 = Vec<i64>;
-type InputPart2 = InputPart1;
-type OutputPart1 = usize;
-type OutputPart2 = OutputPart1;
+type Input = Vec<i64>;
+type Output = usize;
 
-#[aoc_generator(day1, part1)]
-pub fn input_generator_part1(raw_input: &str) -> InputPart1 {
+// ========================================
+// Original solution
+// ========================================
+
+#[aoc_generator(day1, part1, Original)]
+pub fn input_generator_part1(raw_input: &str) -> Input {
     raw_input
         .lines()
         .map(|line| line.parse().unwrap())
         .collect()
 }
 
-#[aoc_generator(day1, part2)]
-pub fn input_generator_part2(raw_input: &str) -> InputPart2 {
+#[aoc_generator(day1, part2, Original)]
+pub fn input_generator_part2(raw_input: &str) -> Input {
     input_generator_part1(raw_input)
         .windows(3)
         .map(|window| window.iter().sum())
         .collect()
 }
 
-#[aoc(day1, part1)]
-pub fn solve_part1(input: &InputPart1) -> OutputPart1 {
+#[aoc(day1, part1, Original)]
+pub fn solve_part1(input: &Input) -> Output {
     input
         .iter()
         .zip(&input[1..])
@@ -30,7 +32,37 @@ pub fn solve_part1(input: &InputPart1) -> OutputPart1 {
         .count()
 }
 
-#[aoc(day1, part2)]
-pub fn solve_part2(input: &InputPart2) -> OutputPart2 {
+#[aoc(day1, part2, Original)]
+pub fn solve_part2(input: &Input) -> Output {
     solve_part1(input)
+}
+
+// ========================================
+// Clever solution
+// ========================================
+//
+// There is no need to sum the windows, because the two windows in question will
+// share the same middle numbers. We need to compare only the first number to the
+// last number.
+
+#[aoc_generator(day1, Clever)]
+pub fn input_generator_clever(raw_input: &str) -> Input {
+    input_generator_part1(raw_input)
+}
+
+fn solve(input: &Input, window_size: usize) -> Output {
+    input
+        .windows(window_size)
+        .filter(|window| window.last() > window.first())
+        .count()
+}
+
+#[aoc(day1, part1, Clever)]
+pub fn solve_part1_clever(input: &Input) -> Output {
+    solve(input, 2)
+}
+
+#[aoc(day1, part2, Clever)]
+pub fn solve_part2_clever(input: &Input) -> Output {
+    solve(input, 4)
 }
