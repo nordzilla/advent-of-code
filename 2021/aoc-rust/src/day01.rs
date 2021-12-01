3619f67453b53c4e43201f16661f5c29e5060f10
Erik Nordin <nordzilla@mozilla.com>
2021-12-01 10:52:05 -0800

    Refactor Day 01 Solution

--- 2021/aoc-rust/src/day01.rs
+++ 2021/aoc-rust/src/day01.rs
@@ -1,6 +1,6 @@
 use aoc_runner_derive::*;
 
-type InputPart1 = Vec<usize>;
+type InputPart1 = Vec<i64>;
 type InputPart2 = InputPart1;
 type OutputPart1 = usize;
 type OutputPart2 = OutputPart1;
@@ -16,6 +16,9 @@ pub fn input_generator_part1(raw_input: &str) -> InputPart1 {
 #[aoc_generator(day1, part2)]
 pub fn input_generator_part2(raw_input: &str) -> InputPart2 {
     input_generator_part1(raw_input)
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect()
 }
 
 #[aoc(day1, part1)]
@@ -23,20 +26,11 @@ pub fn solve_part1(input: &InputPart1) -> OutputPart1 {
     input
         .iter()
         .zip(&input[1..])
-        .filter(|(lhs, rhs)| lhs < rhs)
+        .filter(|(prev, next)| next > prev)
         .count()
 }
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &InputPart2) -> OutputPart2 {
-    input
-        .windows(3)
-        .map(|window| window.iter().sum::<usize>())
-        .zip(
-            input[1..]
-                .windows(3)
-                .map(|window| window.iter().sum::<usize>()),
-        )
-        .filter(|(lhs, rhs)| lhs < rhs)
-        .count()
+    solve_part1(input)
 }
