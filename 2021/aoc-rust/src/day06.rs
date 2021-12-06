c47b6cb5ddb76e8f8a8e8cf747a287842f2901eb
Erik Nordin <nordzilla@mozilla.com>
2021-12-06 00:11:54 -0800

    2021 Day 06 Rust (Refactor)

--- 2021/aoc-rust/src/day06.rs
+++ 2021/aoc-rust/src/day06.rs
@@ -1,50 +1,34 @@
-use std::{cell::RefCell, collections::HashMap};
-
 use aoc_runner_derive::*;
-use flow_control::return_if;
 
-type Input = Vec<i64>;
-type Output = i64;
-
-thread_local! {
-    static SPAWN_CACHE: RefCell<HashMap<(i64, i64), i64>> = RefCell::new(HashMap::new());
-}
-
-fn spawn_count(days_left: i64, timer: i64) -> i64 {
-    return_if!(days_left - timer <= 0, 1);
-    if let Some(count) = SPAWN_CACHE.with(|cache| cache.borrow().get(&(days_left, timer)).copied())
-    {
-        return count;
-    }
-    let count = 1
-        + (0..days_left - timer)
-            .rev()
-            .step_by(7)
-            .map(|days_left| spawn_count(days_left, 8))
-            .sum::<i64>();
-    SPAWN_CACHE.with(|cache| {
-        cache
-            .borrow_mut()
-            .entry((days_left, timer))
-            .or_insert(count)
-            .clone()
-    })
-}
+type Input = [u64; 9];
+type Output = u64;
 
 #[aoc_generator(day6)]
-fn input_generator(raw_input: &str) -> Input {
+fn input_generator_arrays(raw_input: &str) -> Input {
     raw_input
         .split(',')
-        .map(|n| n.parse::<i64>().unwrap())
-        .collect()
+        .map(|n| n.parse::<usize>().unwrap())
+        .fold([0; 9], |mut arr, timer| {
+            arr[timer] += 1;
+            arr
+        })
+}
+
+fn spawn_count(fish: [u64; 9], days_left: u64) -> u64 {
+    (0..days_left)
+        .fold(fish, |[t0, t1, t2, t3, t4, t5, t6, t7, t8], _| {
+            [t1, t2, t3, t4, t5, t6, t7 + t0, t8, t0]
+        })
+        .into_iter()
+        .sum()
 }
 
 #[aoc(day6, part1)]
-fn solve_part1(input: &Input) -> Output {
-    input.iter().map(|&timer| spawn_count(80, timer)).sum()
+fn solve_part1(&input: &Input) -> Output {
+    spawn_count(input, 80)
 }
 
 #[aoc(day6, part2)]
-fn solve_part2(input: &Input) -> Output {
-    input.iter().map(|&timer| spawn_count(256, timer)).sum()
+fn solve_part2(&input: &Input) -> Output {
+    spawn_count(input, 256)
 }
