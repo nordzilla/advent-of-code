1fa1ab2b6dceee5bd7583357a4563ce08e49b703
Erik Nordin <nordzilla@mozilla.com>
2021-12-06 00:11:54 -0800

    2021 Day 06 Rust (Refactor)

--- 2021/aoc-rust/src/day06.rs
+++ 2021/aoc-rust/src/day06.rs
@@ -1,42 +1,37 @@
-use std::{cell::RefCell, collections::HashMap};
-
 use aoc_runner_derive::*;
 use flow_control::return_if;
+use std::{cell::RefCell, collections::HashMap};
 
-type Input = Vec<i64>;
+type Input = Vec<i16>;
 type Output = i64;
 
 thread_local! {
-    static SPAWN_CACHE: RefCell<HashMap<(i64, i64), i64>> = RefCell::new(HashMap::new());
+    static FISHIES_CACHE: RefCell<HashMap<(i16, i16), i64>> = RefCell::new(HashMap::new());
 }
 
-fn spawn_count(days_left: i64, timer: i64) -> i64 {
+#[aoc_generator(day6)]
+fn input_generator(raw_input: &str) -> Input {
+    raw_input.split(',').map(|n| n.parse().unwrap()).collect()
+}
+
+fn spawn_count(days_left: i16, timer: i16) -> i64 {
     return_if!(days_left - timer <= 0, 1);
-    if let Some(count) = SPAWN_CACHE.with(|cache| cache.borrow().get(&(days_left, timer)).copied())
-    {
-        return count;
-    }
-    let count = 1
-        + (0..days_left - timer)
-            .rev()
-            .step_by(7)
-            .map(|days_left| spawn_count(days_left, 8))
-            .sum::<i64>();
-    SPAWN_CACHE.with(|cache| {
+    let count = FISHIES_CACHE
+        .with(|cache| cache.borrow().get(&(days_left, timer)).copied())
+        .unwrap_or_else(|| {
+            1 + (0..days_left - timer)
+                .rev()
+                .step_by(7)
+                .map(|days_left| spawn_count(days_left, 8))
+                .sum::<i64>()
+        });
+    FISHIES_CACHE.with(|cache| {
         cache
             .borrow_mut()
             .entry((days_left, timer))
             .or_insert(count)
             .clone()
     })
-}
-
-#[aoc_generator(day6)]
-fn input_generator(raw_input: &str) -> Input {
-    raw_input
-        .split(',')
-        .map(|n| n.parse::<i64>().unwrap())
-        .collect()
 }
 
 #[aoc(day6, part1)]
