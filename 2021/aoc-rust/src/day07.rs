60e491a7ad5d0f5a5ba401a1907eef9d0107951a
Erik Nordin <nordzilla@mozilla.com>
2021-12-06 23:43:39 -0800

    2021 Day 07 Rust (Refactor)

--- 2021/aoc-rust/src/day07.rs
+++ 2021/aoc-rust/src/day07.rs
@@ -1,41 +1,52 @@
 use aoc_runner_derive::*;
+use itertools::Itertools;
 
 type Input = Vec<i64>;
 type Output = i64;
 
 #[aoc_generator(day7)]
 fn input_generator_arrays(raw_input: &str) -> Input {
-    raw_input.split(',').map(|n| n.parse().unwrap()).collect()
+    raw_input
+        .split(',')
+        .map(|n| n.parse().unwrap())
+        .sorted()
+        .collect()
+}
+
+#[derive(Clone, Copy)]
+enum CostType {
+    Constant,
+    Increasing,
 }
 
 fn iter_expanding_from(n: i64) -> impl Iterator<Item = (i64, i64)> {
     (1..).map(move |dist| (n.saturating_sub(dist), n.saturating_add(dist)))
 }
 
-fn fuel_efficiency(input: &Input, target: i64, increasing_cost: bool) -> i64 {
+fn fuel_efficiency(input: &Input, target: i64, cost_type: CostType) -> i64 {
     input
         .iter()
         .map(|&location| {
             let cost = (location - target).abs();
-            increasing_cost
-                .then(|| (cost * (cost + 1)) / 2)
-                .unwrap_or(cost)
+            match cost_type {
+                CostType::Constant => cost,
+                CostType::Increasing => cost * (cost + 1) / 2,
+            }
         })
         .sum()
 }
 
-fn fuel_efficient_convergence(input: &Input, increasing_cost: bool) -> i64 {
-    let average = input.iter().sum::<i64>() / input.len() as i64;
-    let (mut left_min, mut right_min) = (
-        fuel_efficiency(input, average, increasing_cost),
-        fuel_efficiency(input, average, increasing_cost),
-    );
-    for (left_cost, right_cost) in iter_expanding_from(average).map(|(next_left, next_right)| {
-        (
-            fuel_efficiency(input, next_left, increasing_cost),
-            fuel_efficiency(input, next_right, increasing_cost),
-        )
-    }) {
+fn fuel_efficient_convergence(input: &Input, start_point: i64, cost_type: CostType) -> i64 {
+    let mut left_min = fuel_efficiency(input, start_point, cost_type);
+    let mut right_min = left_min;
+    for (left_cost, right_cost) in
+        iter_expanding_from(start_point).map(|(next_left, next_right)| {
+            (
+                fuel_efficiency(input, next_left, cost_type),
+                fuel_efficiency(input, next_right, cost_type),
+            )
+        })
+    {
         flow_control::break_if!(left_cost > left_min && right_cost > right_min);
         (left_cost < left_min).then(|| left_min = left_cost);
         (right_cost < right_min).then(|| right_min = right_cost);
@@ -45,10 +56,12 @@ fn fuel_efficient_convergence(input: &Input, increasing_cost: bool) -> i64 {
 
 #[aoc(day7, part1)]
 fn solve_part1(input: &Input) -> Output {
-    fuel_efficient_convergence(input, false)
+    let median = input.iter().copied().nth(input.len() / 2).unwrap();
+    fuel_efficient_convergence(input, median, CostType::Constant)
 }
 
 #[aoc(day7, part2)]
 fn solve_part2(input: &Input) -> Output {
-    fuel_efficient_convergence(input, true)
+    let average = input.iter().sum::<i64>() / input.len() as i64;
+    fuel_efficient_convergence(input, average, CostType::Increasing)
 }
