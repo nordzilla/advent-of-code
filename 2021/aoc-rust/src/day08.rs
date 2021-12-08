9daf1b53d32c4a9415332b7fe3df624aaebdd4b6
Erik Nordin <nordzilla@mozilla.com>
2021-12-08 01:55:27 -0800

    2021 Day 08 Rust (Refactor)

--- 2021/aoc-rust/src/day08.rs
+++ 2021/aoc-rust/src/day08.rs
@@ -1,134 +1,116 @@
 use aoc_runner_derive::*;
+use flow_control::return_if;
 
-type Input = Vec<(Vec<u8>, Vec<u8>)>;
+type Input = Vec<Vec<u8>>;
 type Output = usize;
 
-const BIMAP_LEN: usize = 138;
-type BiMap = [u8; BIMAP_LEN];
+// The counts of how many segments are turned on for the seven-segment
+// representations of the digits.
+const ON0: u32 = 6;
+const ON1: u32 = 2;
+const ON2: u32 = 5;
+const ON3: u32 = 5;
+const ON4: u32 = 4;
+const ON5: u32 = 5;
+const ON6: u32 = 6;
+const ON7: u32 = 3;
+const ON8: u32 = 7;
+const ON9: u32 = 6;
 
-/// Values for collecting segment sequences into a single u8 representation.
-#[rustfmt::skip]
-static SEGMENT_BITS: [u8; 7] = [
-    1 << 0,
-    1 << 1,
-    1 << 2,
-    1 << 3,
-    1 << 4,
-    1 << 5,
-    1 << 6,
-];
+// The products of the counts of segments that are turned on
+// for each digit that includes the segment. These products
+// end up mapping to distinct values for each segment.
+const A: u32 = ON0 * ON2 * ON3 * ON5 * ON6 * ON7 * ON8 * ON9;
+const B: u32 = ON0 * ON4 * ON5 * ON6 * ON8 * ON9;
+const C: u32 = ON0 * ON1 * ON2 * ON3 * ON4 * ON7 * ON8 * ON9;
+const D: u32 = ON2 * ON3 * ON4 * ON5 * ON6 * ON8 * ON9;
+const E: u32 = ON0 * ON2 * ON6 * ON8;
+const F: u32 = ON0 * ON1 * ON3 * ON4 * ON5 * ON6 * ON7 * ON8 * ON9;
+const G: u32 = ON0 * ON2 * ON3 * ON5 * ON6 * ON8 * ON9;
 
-/// Values guaranteed to not be a u8 sequence mapping. Since the seven-pin
-/// mappings are contained within the first seven bits, all of these values
-/// have the 8th bit set.
-static SEQUENCE_KEY: [usize; 10] = [
-    0 | 128,
-    1 | 128,
-    2 | 128,
-    3 | 128,
-    4 | 128,
-    5 | 128,
-    6 | 128,
-    7 | 128,
-    8 | 128,
-    9 | 128,
-];
+// The sums of the above weighted products for each segment that is
+// required to light up each digit's sven-segment representation.
+// These sums end up mapping to distinct values for each seven-segment digit.
+const DIGIT0: u32 = A + B + C + E + F + G;
+const DIGIT1: u32 = C + F;
+const DIGIT2: u32 = A + C + D + E + G;
+const DIGIT3: u32 = A + C + D + F + G;
+const DIGIT4: u32 = B + C + D + F;
+const DIGIT5: u32 = A + B + D + F + G;
+const DIGIT6: u32 = A + B + D + E + F + G;
+const DIGIT7: u32 = A + C + F;
+const DIGIT8: u32 = A + B + C + D + E + F + G;
+const DIGIT9: u32 = A + B + C + D + F + G;
 
-fn parse_sequences<'a>(mut iter: impl Iterator<Item = &'a str>) -> Vec<u8> {
+// We create an array to accumulate the products for each turned-on segment
+// multiplied by the length of the sequence it was contained in.
+//
+// In the end we will end up with some permutation of the [A, B, C, D, E, F, G]
+// constants above that is automatically mapped to the correct indices when treating
+// (b'a' as 0), (b'b' as 1), (b'c' as 2), and so on for indexing.
+//
+// Once we have this mapping, we need to just sum the values at the appropriate
+// indices for each segment that is present in the observed output sequence.
+//
+// This will produce one of the DIGIT0, DIGIT1, DIGIT2... constants above.
+fn parse_segment_map<'a>(mut iter: impl Iterator<Item = &'a str>) -> [u32; 7] {
+    let mut segment_map = [1; 7];
     iter.next()
         .unwrap()
         .split_whitespace()
-        .map(|sequence| {
-            sequence
-                .bytes()
-                .map(|byte| SEGMENT_BITS[(byte - b'a') as usize])
-                .fold(0, |byte, bit| byte | bit)
-        })
-        .collect()
+        .for_each(|sequence| {
+            sequence.bytes().for_each(|byte| {
+                segment_map[(byte - b'a') as usize] *= sequence.len() as u32;
+            });
+        });
+    segment_map
+}
+
+// Once we have a weighted sum that maps to one of the above DIGIT0, DIGIT1... constants,
+// we can map that directly to its integer value.
+fn to_digit(weighted_segment_sum: u32) -> u8 {
+    return_if!(weighted_segment_sum == DIGIT0, 0);
+    return_if!(weighted_segment_sum == DIGIT1, 1);
+    return_if!(weighted_segment_sum == DIGIT2, 2);
+    return_if!(weighted_segment_sum == DIGIT3, 3);
+    return_if!(weighted_segment_sum == DIGIT4, 4);
+    return_if!(weighted_segment_sum == DIGIT5, 5);
+    return_if!(weighted_segment_sum == DIGIT6, 6);
+    return_if!(weighted_segment_sum == DIGIT7, 7);
+    return_if!(weighted_segment_sum == DIGIT8, 8);
+    return_if!(weighted_segment_sum == DIGIT9, 9);
+    unreachable!("invalid segment sum");
 }
 
 #[aoc_generator(day8)]
-fn input_generator_arrays(raw_input: &str) -> Input {
+fn input_generator(raw_input: &str) -> Input {
     raw_input
         .lines()
         .map(|line| {
             let mut split = line.split(" | ");
-            (parse_sequences(&mut split), parse_sequences(&mut split))
+            let segment_map = parse_segment_map(&mut split);
+            split
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(move |sequence| {
+                    sequence
+                        .bytes()
+                        .map(|byte| segment_map[(byte - b'a') as usize])
+                        .sum::<u32>()
+                })
+                .map(to_digit)
+                .collect()
         })
         .collect()
-}
-
-/// Takes a digit and its expected pop count as a segment sequence.
-fn decode_unique(digit: usize, pop_count: u32, sequences: &[u8], bi_map: &mut BiMap) {
-    let sequence = sequences
-        .iter()
-        .copied()
-        .find(|sequence| sequence.count_ones() == pop_count)
-        .unwrap();
-    bi_map[SEQUENCE_KEY[digit]] = sequence;
-    bi_map[sequence as usize] = digit as u8;
-}
-
-fn xor_pop_count(lhs: u8, rhs: u8) -> u32 {
-    (lhs ^ rhs).count_ones()
-}
-
-/// Takes a digit and its expected segment pop-count when xored with
-/// 1, 4, 7, and 8 respectively as segment sequences.
-///
-/// Xoring the the segments sequences that have non-unique counts produces
-/// a distinct mapping when xored with the 1, 4, 7, and 8's segment sequences.
-fn decode_from_known(
-    digit: usize,
-    [xor_pop1, xor_pop4, xor_pop7, xor_pop8]: [u32; 4],
-    sequences: &[u8],
-    bi_map: &mut BiMap,
-) {
-    let sequence = sequences
-        .iter()
-        .copied()
-        .find(|&sequence| {
-            xor_pop_count(sequence, bi_map[SEQUENCE_KEY[1]]) == xor_pop1
-                && xor_pop_count(sequence, bi_map[SEQUENCE_KEY[4]]) == xor_pop4
-                && xor_pop_count(sequence, bi_map[SEQUENCE_KEY[7]]) == xor_pop7
-                && xor_pop_count(sequence, bi_map[SEQUENCE_KEY[8]]) == xor_pop8
-        })
-        .unwrap();
-    bi_map[SEQUENCE_KEY[digit]] = sequence;
-    bi_map[sequence as usize] = digit as u8;
-}
-
-fn decode(sequences: &[u8]) -> BiMap {
-    let mut bi_map = [0; BIMAP_LEN];
-
-    // Get the numbers with unique pins.
-    decode_unique(1, 2, sequences, &mut bi_map);
-    decode_unique(4, 4, sequences, &mut bi_map);
-    decode_unique(7, 3, sequences, &mut bi_map);
-    decode_unique(8, 7, sequences, &mut bi_map);
-
-    // Derive other numbers by xoring with the ones above.
-    decode_from_known(0, [4, 4, 3, 1], sequences, &mut bi_map);
-    decode_from_known(2, [5, 5, 4, 2], sequences, &mut bi_map);
-    decode_from_known(3, [3, 3, 2, 2], sequences, &mut bi_map);
-    decode_from_known(5, [5, 3, 4, 2], sequences, &mut bi_map);
-    decode_from_known(6, [6, 4, 5, 1], sequences, &mut bi_map);
-    decode_from_known(9, [4, 2, 3, 1], sequences, &mut bi_map);
-
-    bi_map
 }
 
 #[aoc(day8, part1)]
 fn solve_part1(input: &Input) -> Output {
     input
         .iter()
-        .flat_map(|(notes, outputs)| {
-            let bi_map = decode(notes);
-            outputs
-                .iter()
-                .map(move |&sequence| bi_map[sequence as usize])
-        })
-        .filter(|&n| n == 1 || n == 4 || n == 7 || n == 8)
+        .flatten()
+        .filter(|&&n| n == 1 || n == 4 || n == 7 || n == 8)
         .count()
 }
 
@@ -136,14 +118,12 @@ fn solve_part1(input: &Input) -> Output {
 fn solve_part2(input: &Input) -> Output {
     input
         .iter()
-        .flat_map(|(notes, outputs)| {
-            let bi_map = decode(notes);
+        .flat_map(|outputs| {
             outputs
                 .iter()
                 .rev()
-                .map(move |&sequence| bi_map[sequence as usize] as usize)
                 .zip((0..).map(|n| 10_usize.pow(n)))
-                .map(|(digit, place_value)| digit * place_value)
+                .map(|(&digit, place_value)| digit as usize * place_value)
         })
         .sum()
 }
