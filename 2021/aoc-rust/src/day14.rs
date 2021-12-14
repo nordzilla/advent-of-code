04f80ddbd206034e4d79c1f1954511ff7a7e3769
Erik Nordin <nordzilla@mozilla.com>
2021-12-13 23:41:52 -0800

    2021 Day 14 Rust (Refactor)

--- 2021/aoc-rust/src/day14.rs
+++ 2021/aoc-rust/src/day14.rs
@@ -1,93 +1,93 @@
 use aoc_runner_derive::*;
+use flow_control::{continue_if, return_if};
 
-const fn get_index(lhs: usize, rhs: usize) -> usize {
-    lhs << 7 | rhs
+const MAX_INDEX: usize = single_index(b'Z' as u16);
+const MAX_PAIR_INDEX: usize = combined_index(b'Z' as u16, b'Z' as u16);
+type CountsMap = [u64; MAX_INDEX + 1];
+type PairsMap = [(u64, Option<u16>); MAX_PAIR_INDEX + 1];
+type Input = (CountsMap, PairsMap);
+type Output = u64;
+
+const fn single_index(c: u16) -> usize {
+    (c - (b'A' as u16)) as usize
 }
 
-const fn get_pair(index: usize) -> (usize, usize) {
-    ((index >> 7) & 0b1111111, index & 0b1111111)
+const fn combined_index(c1: u16, c2: u16) -> usize {
+    ((c1 - (b'A' as u16)) << 5 | (c2 - (b'A' as u16))) as usize
 }
 
-#[test]
-fn test_index_roundtrips() {
-    for lhs in b'A'..=b'Z' {
-        let lhs = lhs as usize;
-        for rhs in b'A'..=b'Z' {
-            let rhs = rhs as usize;
-            assert_eq!(get_pair(get_index(lhs, rhs)), (lhs, rhs));
-        }
-    }
+const fn pair_from(index: usize) -> (u16, u16) {
+    (
+        ((index as u16 >> 5) & 0b11111) + b'A' as u16,
+        (index as u16 & 0b11111) + b'A' as u16,
+    )
 }
 
-const MIN_INDEX: usize = get_index('A' as usize, 'A' as usize);
-const MAX_INDEX: usize = get_index('Z' as usize, 'Z' as usize);
-type Input = [(usize, Option<usize>); MAX_INDEX + 1];
-type Output = usize;
-
-#[aoc_generator(day14)]
-fn map_generator(raw_map: &str) -> Input {
-    let mut map = [(0, None); MAX_INDEX + 1];
+#[aoc_generator(day14, part1, nordzilla)]
+#[aoc_generator(day14, part2, nordzilla)]
+fn input_generator(raw_map: &str) -> Input {
+    let mut counts = [0; MAX_INDEX + 1];
+    let mut pairs = [(0, None); MAX_PAIR_INDEX + 1];
     let mut iter = raw_map.split("\n\n");
     let seed = iter
         .next()
         .unwrap()
         .bytes()
-        .map(|byte| byte as usize)
+        .map(|byte| byte as u16)
         .collect::<Vec<_>>();
     for &byte in &seed {
-        map[byte].0 += 1;
+        counts[single_index(byte)] += 1;
     }
     for w in seed.windows(2) {
-        map[get_index(w[0], w[1])].0 += 1;
+        pairs[combined_index(w[0], w[1])].0 += 1;
     }
     iter.next().unwrap().lines().for_each(|line| {
         let mut iter = line.split(" -> ");
         let pair = iter.next().unwrap();
-        let c1 = pair.as_bytes().get(0).copied().unwrap() as usize;
-        let c2 = pair.as_bytes().get(1).copied().unwrap() as usize;
-        let c3 = iter.next().unwrap().chars().next().unwrap() as usize;
-        let idx = get_index(c1, c2);
-        map[idx].1 = Some(c3);
+        let c1 = pair.as_bytes().get(0).copied().unwrap() as u16;
+        let c2 = pair.as_bytes().get(1).copied().unwrap() as u16;
+        let c3 = iter.next().unwrap().chars().next().unwrap() as u16;
+        pairs[combined_index(c1, c2)].1 = Some(c3);
     });
-    map
+    (counts, pairs)
 }
 
-fn step(in_map: Input) -> Input {
-    let mut out_map = in_map;
-    for i in MIN_INDEX..=MAX_INDEX {
-        if let (count, Some(insertion)) = in_map[i] {
-            let (c1, c2) = get_pair(i);
-            out_map[i].0 -= count;
-            out_map[insertion].0 += count;
-            out_map[get_index(c1, insertion)].0 += count;
-            out_map[get_index(insertion, c2)].0 += count;
+fn step(counts: &mut CountsMap, in_pairs: &PairsMap) -> PairsMap {
+    let mut out_pairs = *in_pairs;
+    for index in 0..=MAX_PAIR_INDEX {
+        if let (count, Some(c3)) = in_pairs[index] {
+            continue_if!(count == 0);
+            let (c1, c2) = pair_from(index);
+            out_pairs[index].0 -= count;
+            out_pairs[combined_index(c1, c3)].0 += count;
+            out_pairs[combined_index(c3, c2)].0 += count;
+            counts[single_index(c3)] += count;
         }
     }
-    out_map
+    out_pairs
 }
 
-fn max_minus_min(map: &Input) -> usize {
-    let max = map['A' as usize..='Z' as usize]
+fn max_minus_min(counts: &CountsMap) -> u64 {
+    let (min, max) = counts
         .iter()
-        .map(|&(count, _)| count)
-        .filter(|&count| count > 0)
-        .max()
-        .unwrap();
-    let min = map['A' as usize..='Z' as usize]
-        .iter()
-        .map(|&(count, _)| count)
-        .filter(|&count| count > 0)
-        .min()
-        .unwrap();
+        .fold((u64::MAX, u64::MIN), |(min, max), &value| {
+            return_if!(value == 0, (min, max));
+            (
+                if value < min { value } else { min },
+                if value > max { value } else { max },
+            )
+        });
     max - min
 }
 
-#[aoc(day14, part1)]
-fn solve_part1(&map: &Input) -> Output {
-    max_minus_min(&(0..10).fold(map, |map, _| step(map)))
+#[aoc(day14, part1, nordzilla)]
+fn solve_part1(&(mut counts, pairs): &Input) -> Output {
+    (0..10).fold(pairs, |pairs, _| step(&mut counts, &pairs));
+    max_minus_min(&counts)
 }
 
-#[aoc(day14, part2)]
-fn solve_part2(&map: &Input) -> Output {
-    max_minus_min(&(0..40).fold(map, |map, _| step(map)))
+#[aoc(day14, part2, nordzilla)]
+fn solve_part2(&(mut counts, pairs): &Input) -> Output {
+    (0..40).fold(pairs, |pairs, _| step(&mut counts, &pairs));
+    max_minus_min(&counts)
 }
