728316b5881f634fc9e978ac5d6b7864be8f6df1
Erik Nordin <nordzilla@mozilla.com>
2024-12-01 10:17:58 -0600

    Add AOC-2024 Project

---
+++ 2024/aoc-rust/src/day-template.rs
@@ -0,0 +1,21 @@
+use aoc_runner_derive::*;
+use text_io::scan;
+
+type Input = String;
+type Output = u64;
+
+#[aoc_generator(day2, part1, Original)]
+#[aoc_generator(day2, part2, Original)]
+fn input_generator(raw_input: &str) -> Input {
+    raw_input.to_string()
+}
+
+#[aoc(day2, part1, Original)]
+fn solve_part1(_: &Input) -> Output {
+    0
+}
+
+#[aoc(day2, part2, Original)]
+fn solve_part2(_: &Input) -> Output {
+    0
+}
